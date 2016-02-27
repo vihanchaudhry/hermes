c609064add39500e5773a6335c6b7fd10a675bac
Vihan Chaudhry <vihanchaudhry@gmail.com>
2016-02-27 09:49:55 -0800

    Initial commit

---
+++ hermes.py
@@ -0,0 +1,24 @@
+from utils import geo
+from utils import lyft
+from run import app
+
+# get ArcGIS API token
+arcgis_token = geo.get_token()
+
+# geocode address - get address from frontend
+locations = geo.geocode(arcgis_token, "Empire State Building")
+print locations.json()
+
+# get 2-legged lyft token
+lyft_public_token = lyft.get_public_token()
+print lyft_public_token.json()
+
+# get cost and ETA
+eta = lyft.get_eta(lyft_public_token, locations)
+print eta.json()
+
+# get 3-legged lyft token
+
+# request lyft
+
+app.run(debug=True, use_reloader=False)
